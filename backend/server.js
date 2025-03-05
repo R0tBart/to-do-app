d9239223fc183a8402ab1d7fe75383a908259a6e
Chris | R0tBart | techstarter <christoph.von.hagen@tn.techstarter.de>
2025-03-05 12:56:32 +0100

    backend und server geupdated

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,37 @@
 const express = require('express');
-
+const sqlite3 = require('sqlite3');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+//TODO: Verbinde eine Datenbank dazu
 
+const db = new sqlite3.Database('./tasks.db');
+
+app.use(bodyParser.json());     // Middleware
+
+
+db.run('CREATE TABLE IF NOT EXISTS tasks (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, completed BOOLEAN DEFAULT 0)');
+
+
+//TODO: Schreibe requests/responses
 
 
 app.get('/', (req, res) => {
+    res.send('request received');
+});
 
-    res.send('Request received!');
+app.get('/ralf', (req, res) => {
+    res.send('vielen Dank Ralf');
+});
+
+app.post('/add', (req, res) => {
+    db.run('INSERT INTO tasks (title) VALUES (?)', [req.body.title], function () {
+        res.json({tag: "Mittwoch", bald_wirds: "Mittagspause"});
+    });
 });
 
 
-app.get('/chris', (req, res) => {
-    res.send('Chris antwortet!');
-});
-
-app.listen(3050, 'localhost', ()=> {
-    console.log('Server running on port 3050');
+app.listen(3050, "localhost", () => {
+    console.log("bald wird es Mittagspause")
 });
