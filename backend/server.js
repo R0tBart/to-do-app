c1f70e5784414fce0016a0ed15a2772b6e4e099b
Chris | R0tBart | techstarter <christoph.von.hagen@tn.techstarter.de>
2025-03-14 16:27:01 +0100

    komments

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,25 @@
+// Benötigte Module werden importiert.
+// Express wird verwendet, um den Server zu erstellen und zu verwalten.
+// Body-Parser wird verwendet, um JSON-Daten zu verarbeiten.
+// CORS wird verwendet, um die Verbindung zwischen dem Frontend und dem Backend zu aktivieren.
+// Pool wird verwendet, um die Datenbankverbindung zu teilen und zu verwalten.
+// Die .env-Datei wird verwendet, um die Datenbankverbindung zu speichern.
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const { Pool } = require('pg');
 require('dotenv').config();
 
-
+// Der Server wird erstellt und auf dem Port 3050 gestartet.
+// Der Server wird auf Anfragen warten und die Anfragen verarbeiten.
 const app = express();
 const PORT = 3050;
 const HOST = 'localhost';
 
+// Datenbankverbindung, die in .env-Datei definiert ist mit postgresql.
+// Die Datenbankverbindung wird in einer Pool-Instanz gespeichert.
+// Die Pool-Instanz wird verwendet, um die Datenbankverbindung zu teilen und zu verwalten.
 
 const pool = new Pool({
     user: process.env.DB_USERNAME,
@@ -18,39 +29,50 @@ const pool = new Pool({
     port: process.env.DB_PORT,
 });
 
-
+// Middleware, um JSON-Daten zu verarbeiten und CORS zu aktivieren 
 app.use(bodyParser.json());
 app.use(cors());
 
+// Startseite
 app.get('/', async (req, res) => {
     res.send('Request received');
 });
 
+// Liste abrufen und senden an den Client sowie in der Konsole anzeigen zu lassen 
 app.get('/liste_abrufen', async (req, res) => {  
     const result = await pool.query('SELECT * FROM tasks');
     res.json(result.rows);
 });
 
-
+// Task hinzufügen und senden an den Client sowie in der Konsole anzeigen zu lassen
 app.post('/add', async (req, res) => {
     console.log('POST kommt an!');
-    const result = await pool.query('INSERT INTO tasks (title) VALUES ($1)', [req.body.title]);
-    console.log(result.rows)
-    res.json(result.rows);
+    const result = await pool.query('INSERT INTO tasks (title) VALUES ($1) RETURNING *', [req.body.title]);
+    res.json(result.rows[0]);
 });
 
+// Tasks aktualisieren und senden an den Client sowie in der Konsole anzeigen zu lassen
+// PUT-Methode wird verwendet, um eine Ressource zu aktualisieren oder zu ersetzen.
+// In diesem Fall wird die Ressource, die aktualisiert wird, durch die ID identifiziert.
 app.put('/update/:id', async (req, res) => {
     console.log('PUT kommt an!');
     const result = await pool.query('UPDATE tasks SET title = $1 WHERE id = $2', [req.body.title, req.params.id]);
     res.json(result.rows);
 });
-app.delete('/delete/:id', (req, res) => {
+
+// Task löschen und senden an den Client sowie in der Konsole anzeigen zu lassen
+// DELETE-Methode wird verwendet, um eine Ressource zu löschen.
+app.delete('/delete/:id', async (req, res) => {
     console.log('DELETE kommt an!');
     const { id } = req.params;
-    const result = pool.query('DELETE FROM tasks WHERE id = $1', [id]);
+    const result = await pool.query('DELETE FROM tasks WHERE id = $1', [id]);
     res.json(result.rows);
 });
 
+
+// Server starten und auf Anfragen warten 
+// Der Server wird auf dem Port 3050 gestartet und auf Anfragen gewartet.
+// Wenn der Server gestartet wird, wird eine Nachricht in der Konsole ausgegeben.
 app.listen(PORT, HOST, () => {
     console.log(`Server läuft auf http://${HOST}:${PORT}`);
 });
--- frontend/todo-react-app/src/App.jsx
+++ frontend/todo-react-app/src/App.jsx
@@ -1,70 +1,85 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
     const [tasks, setTasks] = useState([]);
-    const [newTask, setNewTask] = useState('');
+    const [newTask, setNewTask] = useState("");
 
-    const addTask = () => {
-        console.log('es läuft');
+    const handleAddTask = () => {
+        console.log("es läuft");
 
-        fetch('http://localhost:3050/add', {
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'},
+        fetch("http://localhost:3050/add", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ title: newTask }),
         })
-        .then(res => res.json())
-        .then(data => setTasks([...tasks, data]))
-        .catch(error => console.error('Error adding task:', error));
-        setNewTask('');
+            .then((res) => res.json())
+            .then((data) => setTasks([...tasks, data]))
+            .catch((error) => console.error("Error adding task:", error));
+        setNewTask("");
     };
 
     const toggleCompletion = (id) => {
-        setTasks(tasks.map(task => {
-            if (task.id === id) {
-                return { ...task, completed: !task.completed };
-            }
-            return task;
-        }));
+        setTasks(
+            tasks.map((task) => {
+                if (task.id === id) {
+                    return { ...task, completed: !task.completed };
+                }
+                return task;
+            })
+        );
+    };
+
+    const handleToggleCompletion = (id) => {
+        toggleCompletion(id);
     };
 
     useEffect(() => {
-        fetch('http://localhost:3050/liste_abrufen')
-            .then(res => res.json())
-            .then(data => setTasks(data))
-            .catch(error => console.error('Error fetching tasks:', error));
+        fetch("http://localhost:3050/liste_abrufen")
+            .then((res) => res.json())
+            .then((data) => setTasks(data))
+            .catch((error) => console.error("Error fetching tasks:", error));
     }, []);
 
     useEffect(() => {
-        console.log('tasks:', tasks);
+        console.log("tasks:", tasks);
     }, [tasks]);
+
+    const handleInputChange = (e) => {
+        setNewTask(e.target.value);
+    };
 
     console.log(tasks);
 
-    const itemLoeschen = (id) => {
-        fetch('http://localhost:3050/delete/' + id, {
-            method: 'DELETE',
-        })
-        .then(() => setTasks(tasks.filter(task => task.id !== id)))
-        .catch(error => console.error('Error deleting task:', error));
+    const handleDelete = (id) => {
+        itemLoeschen(id);
     };
 
+    const itemLoeschen = (id) => {
+        fetch("http://localhost:3050/delete/" + id, {
+            method: "DELETE",
+        })
+            .then(() => setTasks(tasks.filter((task) => task.id !== id)))
+            .catch((error) => console.error("Error deleting task:", error));
+    };
 
     return (
         <>
-            <h1>Todo - Liste mit node and react</h1>
+
+            <h1>Meine To-Do-Liste</h1>
+            <h2>Neue Aufgabe hinzufügen</h2>
             <h2>Was ist zu tun?</h2>
             <h3>Trage es hier ein</h3>
-            <input  type="text" value={newTask} onChange={e => setNewTask(e.target.value)} />
-            <button onClick={addTask} disabled={!newTask.trim()}>Hinzufügen</button>
-            <button onClick={() => setTasks([])}>Clear all</button>
+            <input type="text" value={newTask} onChange={handleInputChange} />
+            <button onClick={handleAddTask} disabled={!newTask.trim()}>Hinzufügen</button>
+            <button onClick={() => {if (window.confirm("BIST DU WIRKLICH SICHER???")) {setTasks([]);}}}>Alle löschen</button>
             <ul>
-            {tasks.map(({id, title, completed}) => (
-                <li key={id}>
-                    <input type="checkbox" checked={completed} onChange={() => toggleCompletion(id)} /> {title}
-                    <button className="delete-button" onClick={() => itemLoeschen(id)}>❌</button>
-                </li>
-            ))}
+                {tasks.map(({ id, title, completed }) => (
+                    <li key={id}>
+                        <input type="checkbox"checked={completed}onChange={() => toggleCompletion(id)}/>{" "}{title}
+                        <button className="delete-button" onClick={() => handleDelete(id)}>❌</button>
+                    </li>
+                ))}
             </ul>
         </>
     );
